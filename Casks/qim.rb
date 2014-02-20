700e9c8b6506706fe8964b6e8087178d07c0a87d
wolfg1969 <wolfg1969@gmail.com>
2014-02-20 15:28:30 +0800

    Add QIM v2.3.0
    QIM(IMKQIM) is a Chinese Pinyin input method running on Mac OS X

---
+++ Casks/qim.rb
@@ -0,0 +1,7 @@
+class Qim < Cask
+  url 'http://www.macqim.com/download/QIM_2_3_0_Installer.zip'
+  homepage 'http://www.macqim.com/‎'
+  version '2.3.0'
+  sha256 '6ff483955ff5d50e2c8ba713e42c78a4134ef93ca0ccae520789a0acd513cc13'
+  install 'Installer.mpkg'
+end
